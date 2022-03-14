774fca4c4f08cca66a713b7d7bee37fb5ac2b30a
Billy Knight <8345283+BillyKoala@users.noreply.github.com>
2022-03-14 23:36:39 +0000

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,87 @@
+"""
+Author: Billy Knight - March 2022.
+
+Project Name: Guess The Random Playing Card.
+
+This is the classes.py file created to reference
+the one single user-defined class in this project.
+"""
+
+from tkinter import *
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import globals
+
+class RandomPlayingCard:
+
+    # This class will be used to record all the properties of the randomly selected playing card.
+
+    def __init__(self,
+                 card_id, card_number, card_suit,
+                 suit_name, suit_colour,
+                 picture_name, card_image_path):
+
+        self.card_id = card_id
+        self.card_number = card_number
+        self.card_suit = card_suit
+        self.suit_name = suit_name
+        self.suit_colour = suit_colour
+        self.picture_name = picture_name
+        self.card_image_path = card_image_path
+        self.joker_image_path = "venv/images/joker2.png"
+        self.joker_jack_image_path = "venv/images/Jack_The_Joker.png"
+        self.card_suits_image_path = "venv/images/card_suits.png"
+        self.developer_mugshot = "venv/images/BK.png"
+        self.developer_mugshot_inv = "venv/images/BK_Inv.png"
+
+        def get_card_id(self):
+            return self.card_id
+
+        def set_card_id(self, card_id):
+            self.card_number = card_id
+
+        def get_card_number(self):
+            return self.card_number
+
+        def set_card_number(self, card_number):
+            self.card_number = card_number
+
+        def get_card_suit(self):
+            return self.card_suit
+
+        def set_card_suit(self, card_suit):
+            self.card_suit = card_suit
+
+        def get_suit_name(self):
+            return self.suit_name
+
+        def set_suit_name(self, suit_name):
+            self.suit_name = suit_name
+
+        def get_picture_name(self):
+            return self.picture_name
+
+        def set_picture_name(self, picture_name):
+            self.picture_name = picture_name
+
+        def get_card_image_path(self):
+            return self.card_image_path
+
+        def set_card_image_path(self, image_path):
+               self.card_image_path = card_image_path
+
+    # STATIC VARIABLES
+        def get_suit_colour(self):
+            return self.suit_colour
+
+        def get_joker_image_path(self):
+            return self.joker_image_path
+
+        def get_card_suits_image_path(self):
+            return self.card_suits_image_path_path
+
+        def get_developer_mugshot(self):
+            return self.developer_mugshot
+
+        def get_developer_mugshot_inv(self):
+            return self.developer_mugshot_inv
---
+++ globals.py
@@ -0,0 +1,35 @@
+"""
+Author: Billy Knight - March 2022.
+
+Project Name: Guess The Random Playing Card.
+
+This is the globals.py file created to hold
+all the global variables of this project.
+"""
+global root
+global random_card
+global selected_card
+global attempt_count
+global selected_card_dict
+global card_deck
+global card_click_dict
+global check_result
+global card_image
+global card_path_array
+global card_id
+global card_id_array
+global card_suit
+global card_suit_array
+global card_name
+global card_number
+global card_number_array
+global card_path
+global matrix_frame
+global matrix_info_frame
+global info_frame
+global hint_info_label
+global developer_mugshot
+global developer_mugshot_inv
+global info_label_dict
+global joker_image_path
+global joker_jack_image_path
