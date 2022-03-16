3c0ac0ab22b75a42aeff4acf24d82ba8af957001
Billy Knight <8345283+BillyKoala@users.noreply.github.com>
2022-03-16 21:59:27 +0000

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,24 @@
+"""
+Author: Billy Knight - March 2022.
+
+Project Name: Guess The Random Playing Card.
+
+This is the constants.py file created to hold
+all the constant variables for this project.
+"""
+
+card_number_array = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K","A"]
+card_suit_array = ["S", "D", "H", "C"]
+picture_cards_array = ["A", "J", "Q", "K"]
+developer_mugshot = "venv/images/BK.png"
+developer_mugshot_inv = "venv/images/BK_Inv.png"
+joker_image_path = "venv/images/joker2.png"
+joker_jack_image_path = "venv/images/Jack_The_Joker.png"
+cards_path = "venv/images/playingcards/"
+card_suits_image_path = "venv/images/card_suits.png"
+card_image_path = "venv/images/playingcards/"
+card_reverse_side_image =  "venv/images/PlayingCardBackView.png"
+favicon_path = "venv/images/favicon.ico"
+info_label_dict = {"before":"Getting Cards Ready...",
+                    "after":"Please Select A Playing Card To Get The Game Going",
+                    "header":"Welcome To Guess The Random Playing Card"}
